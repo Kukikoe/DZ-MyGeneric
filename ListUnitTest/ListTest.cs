6b36a66deb6f0e30bcee165a89da82c286bdc01b
Kukikoe <ja.bondareva2014@yandex.ru>
2018-04-22 17:16:59 +0300

    first commit

---
+++ ListUnitTest/ListTest.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCollections;
+
+namespace ListUnitTest
+{
+    [TestClass]
+    public class ListTest
+    {
+        [TestMethod]
+        public void CtorTest()
+        {
+            MyList<int> list = new MyList<int>();
+            Assert.AreNotEqual(null, list);
+            Assert.AreEqual(0, list.Count);
+            Assert.AreEqual(false, list.IsReadOnly);
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            MyList<int> list = new MyList<int>(4);
+            Assert.AreNotEqual(null, list);
+            Assert.AreEqual(0, list.Count);
+            list.Add(10);
+            Assert.AreEqual(1, list.Count);
+            for(int i = 0; i < 100; ++i)
+            {
+                list.Add(i);
+            }
+            Assert.AreEqual(101, list.Count);
+        }
+
+        [TestMethod]
+        public void ClearTest()
+        {
+            MyList<int> list = new MyList<int>();
+            for (int i = 0; i < 100; ++i)
+            {
+                list.Add(i);
+            }
+            Assert.AreEqual(100, list.Count);
+            list.Clear();
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void IndexTest()
+        {
+            int[] arr = { 1, 2, 5, 4, 3, 7, 9 };
+            MyList<int> list = new MyList<int>();
+            foreach(int val in arr)
+            {
+                list.Add(val);
+            }
+            for (int i = 0; i < list.Count; ++i)
+            {
+                Assert.AreEqual(arr[i], list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            MyList<int> list = new MyList<int>() { 1, 2, 5, 4 };
+            MyList<int> list1 = new MyList<int>() { 2, 5, 4 };
+            bool value = list.Remove(1);
+            Assert.AreEqual(true, value);
+        }
+
+        [TestMethod]
+        public void CopyToTest()
+        {
+            MyList<int> list = new MyList<int>() { 1, 2, 5, 4, 6, 4 };
+            int[] arr = new int[6];
+            list.CopyTo(arr, 0);
+
+            for (int i = 0; i < list.Count; ++i)
+            {
+                Assert.AreEqual(arr[i], list[i]);
+            }
+        }
+
+        [TestMethod]
+        public void IndexOfTest()
+        {
+            MyList<int> list = new MyList<int>() { 1, 2, 5, 4 };
+            var index = list.IndexOf(5);
+            Assert.AreEqual(2, index);
+        }
+
+        [TestMethod]
+        public void ContainsTest()
+        {
+            MyList<int> list = new MyList<int>() { 1, 2, 5, 4 };
+            bool value = list.Contains(5);
+            Assert.AreEqual(true, value);
+        }
+
+        [TestMethod]
+        public void RemoveAtTest()
+        {
+            MyList<int> list = new MyList<int>() { 1, 2, 5, 4 };
+            MyList<int> list1 = new MyList<int>() { 2, 5, 4 };
+            list.RemoveAt(0);
+            for (int i = 0; i < list.Count; ++i)
+            {
+                Assert.AreEqual(list[i], list1[i]);
+            }
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            MyList<int> list = new MyList<int>() { 1, 2, 5, 4 };
+            MyList<int> list1 = new MyList<int>() { 2, 5, 4 };
+            list1.Insert(0, 1);
+            for (int i = 0; i < list.Count; ++i)
+            {
+                Assert.AreEqual(list[i], list1[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void OutOfRangeTest_1()
+        {
+            MyList<int> list = new MyList<int>();
+            list[-1] = 100;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void OutOfRangeTest_2()
+        {
+            MyList<int> list = new MyList<int>();
+            int x = list[-1];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void OutOfRangeTest_3()
+        {
+            MyList<int> list = new MyList<int>();
+            int x = list[1];
+        }
+
+
+    }
+}
---
+++ MyCollections/MyList.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollections
+{
+    public class MyList<T> : IList<T>, IEnumerator<int>
+    {
+        #region fields
+        private T[] _arr;
+        private int _count;
+        private int _current_pos;
+        #endregion
+
+        #region properties and indexator
+        public T this[int index]
+        {
+            get
+            {
+                return _arr[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _count) throw new ArgumentOutOfRangeException("");
+                _arr[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public T Current
+        {
+            get
+            {
+                return _arr[_current_pos];
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        int IEnumerator<int>.Current => throw new NotImplementedException();
+        #endregion
+
+        #region ctors
+        public MyList(int capacity = 10)
+        {
+            _arr = new T[capacity];
+            _count = 0;
+            _current_pos = -1;
+        }
+        #endregion
+
+        public void Add(T value)
+        {
+            if (_count == _arr.Length)
+            {
+                T[] tmp = new T[_arr.Length * 2];
+                for (int j = 0; j < _arr.Length; ++j)
+                {
+                    tmp[j] = _arr[j];
+                }
+                _arr = tmp;
+            }
+            _arr[_count++] = value;
+        }
+
+        public void Clear()
+        {
+            _count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            for (int i = 0; i < _count; ++i)
+            {
+                if (_arr[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            for(int i = 0, j = arrayIndex; i < _count; ++i, ++j)
+            {
+                array[j] = _arr[i];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IList<T>)_arr).GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < _count; ++i)
+            {
+                if (_arr[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (_arr.Length == _count)
+            {
+                T[] tmp = new T[_count * 2];
+                for (int i = 0, j = 0; i < _count; ++i, ++j)
+                {
+                    if (i == index)
+                    {
+                        tmp[index++] = item;
+                        ++j;
+                    }
+                    else
+                    {
+                        tmp[j] = _arr[i];
+                    }
+                }
+                _arr = tmp;
+                ++_count;
+            }
+            else
+            {
+                for (int i = _count; i > index; --i)
+                {
+                    _arr[i] = _arr[i - 1];
+                }
+                _arr[index] = item;
+                ++_count;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            for (int i = 0; i < _count; ++i)
+            {
+                if (_arr[i].Equals(item))
+                {
+                    for (int j = i; j < _count; ++j)
+                    {
+                        _arr[j] = _arr[j + 1];
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < _count; ++i)
+            {
+                _arr[i] = _arr[i + 1];
+            }
+           
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IList<T>)_arr).GetEnumerator();
+        }
+
+        public void Dispose()
+        {
+            Reset();
+        }
+
+        public bool MoveNext()
+        {
+            ++_current_pos;
+            return _current_pos < _count;
+        }
+
+        public void Reset()
+        {
+            _current_pos = -1;
+        }
+    }
+}
